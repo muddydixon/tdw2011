fff9957679ef1ac7cb8f7622cd93a9c40020532c
muddydixon <muddydixon@gmail.com>
2011-10-14 15:50:28 +0900

    add stream

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ var express = require('express')
 , config = require('config')
 , io = require('socket.io')
 , qs = require('querystring')
-, http = require('http')
+, https = require('https')
 , OAuth = require('oauth').OAuth
+, base64 = require('base64')
 ;
 
 // Configuration
@@ -66,19 +67,30 @@ app.get('/screen', function(req, res){
 app.listen(app.settings.port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-var oauth = new OAuth(
-  config.twitter.request_token_url
-  , config.twitter.access_token_url
-  , config.twitter.consumer_key
-  , config.twitter.consumer_secret
-  , '1.0a'
-  , config.twitter.callback_url
-  , 'HMAC-SHA1'
-  , null
-  , config.twitter.headers
-);
-var request = oauth.get(
-  'http://stream.twitter.com/1/statuses/sample.json'
-  , config.twitter.access_token
-  , config.twitter.access_token_secret
-);
+
+var req = https.get({
+  host: 'stream.twitter.com'
+  , port: 443
+  , path: '/1/statuses/sample.json'
+  , headers: {
+    'Authorization': 'Basic '+base64.encode('nifty_engineer:9v3qjvra')
+  }
+}, function(res){
+  res.setEncoding('utf8');
+  var buf = '', id, json;
+  res.on('data', function(chunk){
+    buf += chunk;
+    while((id = buf.indexOf('\r\n')) > -1){
+      json = buf.substr(0, id);
+      buf = buf.substr(id + 2);
+      if(json.length > 0){
+        try{
+          io.sockets.emit('tweet', JSON.parse(json));
+        } catch (x) {
+          
+        }
+      }
+    }
+  });
+});
+
