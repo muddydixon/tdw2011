7ce74c0308d8ef6f6eba1afb9025e5c09a0f5def
muddydixon <muddydixon@gmail.com>
2011-11-01 16:22:27 +0900

    add socket enable marker

--- public/js/screen.js
+++ public/js/screen.js
@@ -305,6 +305,7 @@ $(function(){
   // Stream Data
   var socket = io.connect( appUrl + "/", {port: 8080} );
   socket.on( 'tweet', function( tweet ){
+    $('#socket').css({background:'#666'});
     
     var addImgUrl = exchgImg( tweet );
     
